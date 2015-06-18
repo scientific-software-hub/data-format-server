f41ef64151d71fb37363385bc8b74178d986fcc1
khokhria <mail@ingvord.ru>
2015-06-18 16:13:41 +0200

    progress

--- src/test/java/hzg/wpn/tango/DataFormatServerTest.java
+++ src/test/java/hzg/wpn/tango/DataFormatServerTest.java
@@ -11,17 +11,22 @@ import org.tango.client.ez.proxy.TangoProxy;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+
 /**
  * @author khokhria
  * @since 6/17/15.
  */
 public class DataFormatServerTest {
 
-    private DataFormatServer server = new DataFormatServer();
-
+    private DataFormatServer server = spy(new DataFormatServer());
 
     @Before
     public void before() throws Exception {
+        doReturn(Thread.currentThread().getName()).when(server).getClientId();
+
+
         Files.createDirectories(Paths.get(server.getCwd()));
         Files.deleteIfExists(Paths.get(server.getCwd(), "test.h5"));
         server.createFile("test.h5");
